96306621429a4fcfbb2bc620391e012e3cd27db3
Stephen Darling <stephen.darling@outlook.com>
2018-05-12 16:46:59 -0400

    Added conversion logic for all twelve possible conversions
    The user is now able to see a visual response (green, yellow or red) as to whether their conversion is
    correct.
    
    Next steps will be to move the larger functions into separate files and then create tests to ensure that
    they're working correctly.

--- src/App.js
+++ src/App.js
@@ -56,13 +56,115 @@ class App extends Component {
   // Takes the four input values and determines if the user's conversion is correct, incorrect 
   // or invalid
   performConversion(sv, su, cv, cu) {
+    let celsius_value = ''
+    let calculation = ''
     // Returns invalid unless all four variables are passed and not null
     if (sv && su && cv && cu) {
-      return 'correct'
+      if (su === cu) {
+        return 'invalid'
+      }
+      // For each of the 12 possible converstions the function returns either correct or incorrect based
+      // on whether the user's input matches the calculated function output
+      else {
+        if (su === 'c' && cu === 'r') {
+          calculation = this.celciusToRankine(sv)
+          if (Number(cv).toFixed(1) === calculation) { return 'correct' } else { return 'incorrect' }
+        }
+        else if (su === 'r' && cu === 'c') {
+          calculation = this.rankineToCelcius(sv)
+          if (Number(cv).toFixed === calculation) { return 'correct' } else { return 'incorrect' }
+        }
+        else if (su === 'c' && cu === 'f') {
+          calculation = this.celciusToFahrenheit(sv)
+          if (Number(cv).toFixed(1) === calculation) { return 'correct' } else { return 'incorrect' }
+        }
+        else if (su === 'f' && cu === 'c') {
+          calculation = this.fahrenheitToCelcius(sv)
+          if (Number(cv).toFixed(1) === calculation) { return 'correct' } else { return 'incorrect' }
+        }
+        else if (su === 'c' && cu === 'k') {
+          calculation = this.celciusToKelvin(sv)
+          if (Number(cv).toFixed(1) === calculation) { return 'correct' } else { return 'incorrect' }
+        }
+        else if (su === 'k' && cu === 'c') {
+          calculation = this.kelvinToCelcius(sv)
+          if (Number(cv).toFixed(1) === calculation) { return 'correct' } else { return 'incorrect' }
+        }
+        else if (su === 'r' && cu === 'f') {
+          calculation = this.rankineToFahrenheit(sv)
+          if (Number(cv).toFixed(1) === calculation) { return 'correct' } else { return 'incorrect' }
+        }
+        else if (su === 'r' && cu === 'k') {
+          calculation = this.rankineToKelvin(sv)
+          if (Number(cv).toFixed(1) === calculation) { return 'correct' } else { return 'incorrect' }
+        }
+        else if (su === 'f' && cu === 'r') {
+          calculation = this.fahrenheitToRankine(sv)
+          if (Number(cv).toFixed(1) === calculation) { return 'correct' } else { return 'incorrect' }
+        }
+        else if (su === 'k' && cu === 'f') {
+          calculation = this.kelvinToFahrenheit(sv)
+          if (Number(cv).toFixed(1) === calculation) { return 'correct' } else { return 'incorrect' }
+        }
+        else if (su === 'k' && cu === 'r') {
+          calculation = this.kelvinToRankine(sv)
+          if (Number(cv).toFixed(1) === calculation) { return 'correct' } else { return 'incorrect' }
+        }
+      }
     }
     else {
       return 'invalid'
     }
+  }
+
+  // The 12 functions below perform the calculation from a given unit to another given unit. Conversion
+  // units are in the function name
+  celciusToRankine(value) {
+    let rankine = ((Number(value) + 273.15) * 9/5).toFixed(1)
+    return rankine
+  }
+  celciusToFahrenheit(value) {
+    let fahrenheit = (Number(value) * 9/5 + 32).toFixed(1)
+    return fahrenheit
+  }
+  celciusToKelvin(value) {
+    let kelvin = (Number(value) + 273.15).toFixed(1)
+    return kelvin
+  }
+  rankineToCelcius(value) {
+    let celcius = ((Number(value) - 491.67) * 5/9).toFixed(1)
+    return celcius
+  }
+  rankineToFahrenheit(value) {
+    let fahrenheit = (Number(value) - 459.67).toFixed(1)
+    return fahrenheit
+  }
+  rankineToKelvin(value) {
+
+  }
+  fahrenheitToCelcius(value) {
+    let celcius = ((Number(value) - 32) * 5/9).toFixed(1)
+    return celcius
+  }
+  fahrenheitToKelvin(value) {
+    let kelvin = (Number(value) * 5/9).toFixed(1)
+    return kelvin
+  }
+  fahrenheitToRankine(value) {
+    let rankine = (Number(value) + 459.67).toFixed(1)
+    return rankine
+  }
+  kelvinToCelcius(value) {
+    let celcius = (Number(value) - 273.15).toFixed(1)
+    return celcius
+  }
+  kelvinToFahrenheit(value) {
+    let fahrenheit = (Number(value) * 9/5 - 459.67).toFixed(1)
+    return fahrenheit
+  }
+  kelvinToRankine(value) {
+    let rankine = (Number(value) * 9/5).toFixed(1)
+    return rankine
   }
 
   // Updates state with input value and runs checkSuccess for function after state is updated
@@ -96,6 +198,7 @@ class App extends Component {
           <Form.Field control={Select} onChange={this.handleSelect.bind(this)} name="convertedUnit" label='Converted Unit' options={options} placeholder='Unit' />
         </Form.Group>
       </Form>
+      {/* Renders the response telling the user if the inputs are correct, incorrect or invalid */}
       <Message color={this.state.responseColor}>
         <Message.Header className="App">{this.state.response}</Message.Header>
       </Message>
